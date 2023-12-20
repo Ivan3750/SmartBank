234c652e50f345d1fc74a36fb0b843d8065c9411
Ivan3750 <126425359+Ivan3750@users.noreply.github.com>
2023-12-20 18:12:35 +0100

    v5.1

--- script/CardModal.js
+++ script/CardModal.js
@@ -1,14 +1,19 @@
 const cardsBtn = document.querySelector('.cards');
+/* const mobileList = document.querySelector(".mobile-list")
+ */const cardsMobileBtn = document.querySelector('.mobile-cards');
 const modalCard = document.querySelector('.modal__cards');
 const modalClose = document.querySelector('.modal-btn');
 const body = document.querySelector('.body');
 
 cardsBtn.addEventListener("click", function(event){
     event.preventDefault()
-    modalCard.classList.add("show")
     body.style.overflow = "hidden"
+    mobileList.classList.remove("show")
+    modalCard.classList.add("show")
+    console.log("This")
     
 })
+
 
 
 modalClose.addEventListener("click", function(event){
--- script/script.js
+++ script/script.js
@@ -21,7 +21,7 @@ Screen()
 
 
 
-const cardsBtn = document.querySelector('.cards');
+/* const cardsBtn = document.querySelector('.cards');
 const modalCard = document.querySelector('.modal__cards');
 const modalClose = document.querySelector('.modal-btn');
 const body = document.querySelector('.body');
@@ -41,4 +41,4 @@ modalClose.addEventListener("click", function(event){
     body.style.overflow = "visible"
 
 
-})
+}) */
